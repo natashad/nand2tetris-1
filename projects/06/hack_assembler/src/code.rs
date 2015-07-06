c8716fd386089e38cecf9fb6eb7e521b565af583
Natasha Dalal <natashadalal@gmail.com>
2015-07-06 13:55:25 +0530

    Added in ability to read args from command line and read from input, write to output

--- projects/06/hack_assembler/src/code.rs
+++ projects/06/hack_assembler/src/code.rs
@@ -15,46 +15,46 @@ pub enum Instruction {
 impl Code {
     pub fn new( sym : &parser::Instruction) -> Code {
 
-        let mut compMap = HashMap::<String, String>::new();
-        compMap.insert("0".to_string(), "0101010".to_string());
-        compMap.insert("1".to_string(), "0111111".to_string());
-        compMap.insert("-1".to_string(), "0111010".to_string());
-        compMap.insert("D".to_string(), "0001100".to_string());
-        compMap.insert("A".to_string(), "0110000".to_string());
-        compMap.insert("!D".to_string(), "0001101".to_string());
-        compMap.insert("!A".to_string(), "0110001".to_string());
-        compMap.insert("-D".to_string(), "0001111".to_string());
-        compMap.insert("-A".to_string(), "0110011".to_string());
-        compMap.insert("D+1".to_string(), "0011111".to_string());
-        compMap.insert("A+1".to_string(), "0110111".to_string());
-        compMap.insert("D-1".to_string(), "0001110".to_string());
-        compMap.insert("A-1".to_string(), "0110010".to_string());
-        compMap.insert("D+A".to_string(), "0000010".to_string());
-        compMap.insert("D-A".to_string(), "0010011".to_string());
-        compMap.insert("A-D".to_string(), "0000111".to_string());
-        compMap.insert("D&A".to_string(), "0000000".to_string());
-        compMap.insert("D|A".to_string(), "0010101".to_string());
-        compMap.insert("M".to_string(), "1110000".to_string());
-        compMap.insert("!M".to_string(), "1110001".to_string());
-        compMap.insert("-M".to_string(), "1110011".to_string());
-        compMap.insert("M+1".to_string(), "1110111".to_string());
-        compMap.insert("M-1".to_string(), "1110010".to_string());
-        compMap.insert("D+M".to_string(), "1000010".to_string());
-        compMap.insert("D-M".to_string(), "1010011".to_string());
-        compMap.insert("M-D".to_string(), "1000111".to_string());
-        compMap.insert("D&M".to_string(), "1000000".to_string());
-        compMap.insert("D|M".to_string(), "1010101".to_string());
+        let mut comp_map = HashMap::<String, String>::new();
+        comp_map.insert("0".to_string(), "0101010".to_string());
+        comp_map.insert("1".to_string(), "0111111".to_string());
+        comp_map.insert("-1".to_string(), "0111010".to_string());
+        comp_map.insert("D".to_string(), "0001100".to_string());
+        comp_map.insert("A".to_string(), "0110000".to_string());
+        comp_map.insert("!D".to_string(), "0001101".to_string());
+        comp_map.insert("!A".to_string(), "0110001".to_string());
+        comp_map.insert("-D".to_string(), "0001111".to_string());
+        comp_map.insert("-A".to_string(), "0110011".to_string());
+        comp_map.insert("D+1".to_string(), "0011111".to_string());
+        comp_map.insert("A+1".to_string(), "0110111".to_string());
+        comp_map.insert("D-1".to_string(), "0001110".to_string());
+        comp_map.insert("A-1".to_string(), "0110010".to_string());
+        comp_map.insert("D+A".to_string(), "0000010".to_string());
+        comp_map.insert("D-A".to_string(), "0010011".to_string());
+        comp_map.insert("A-D".to_string(), "0000111".to_string());
+        comp_map.insert("D&A".to_string(), "0000000".to_string());
+        comp_map.insert("D|A".to_string(), "0010101".to_string());
+        comp_map.insert("M".to_string(), "1110000".to_string());
+        comp_map.insert("!M".to_string(), "1110001".to_string());
+        comp_map.insert("-M".to_string(), "1110011".to_string());
+        comp_map.insert("M+1".to_string(), "1110111".to_string());
+        comp_map.insert("M-1".to_string(), "1110010".to_string());
+        comp_map.insert("D+M".to_string(), "1000010".to_string());
+        comp_map.insert("D-M".to_string(), "1010011".to_string());
+        comp_map.insert("M-D".to_string(), "1000111".to_string());
+        comp_map.insert("D&M".to_string(), "1000000".to_string());
+        comp_map.insert("D|M".to_string(), "1010101".to_string());
 
 
-        let mut jumpMap = HashMap::<String, String>::new();
-        jumpMap.insert("null".to_string(), "000".to_string());
-        jumpMap.insert("JGT".to_string(), "001".to_string());
-        jumpMap.insert("JEQ".to_string(), "010".to_string());
-        jumpMap.insert("JGE".to_string(), "011".to_string());
-        jumpMap.insert("JLT".to_string(), "100".to_string());
-        jumpMap.insert("JNE".to_string(), "101".to_string());
-        jumpMap.insert("JLE".to_string(), "110".to_string());
-        jumpMap.insert("JMP".to_string(), "111".to_string());
+        let mut jump_map = HashMap::<String, String>::new();
+        jump_map.insert("null".to_string(), "000".to_string());
+        jump_map.insert("JGT".to_string(), "001".to_string());
+        jump_map.insert("JEQ".to_string(), "010".to_string());
+        jump_map.insert("JGE".to_string(), "011".to_string());
+        jump_map.insert("JLT".to_string(), "100".to_string());
+        jump_map.insert("JNE".to_string(), "101".to_string());
+        jump_map.insert("JLE".to_string(), "110".to_string());
+        jump_map.insert("JMP".to_string(), "111".to_string());
 
 
         match sym.instr_type {
@@ -62,7 +62,7 @@ impl Code {
                 Code {instruction : Instruction::A( sym.symbol.clone() )},
             parser::InstructionType::C => {
                 Code { instruction:
-                    Instruction::C(dest(sym.dest.clone()), comp(sym.comp.clone(), compMap), jump(sym.jump.clone(), jumpMap)) }
+                    Instruction::C(dest(sym.dest.clone()), comp(sym.comp.clone(), comp_map), jump(sym.jump.clone(), jump_map)) }
             }
         }
 
@@ -106,13 +106,13 @@ pub fn dest(sym : String) -> String {
         d.push('0');
     }
 
-    if sym.contains('M') {
+    if sym.contains('D') {
         d.push('1');
     } else {
         d.push('0');
     }
 
-    if sym.contains('D') {
+    if sym.contains('M') {
         d.push('1');
     } else {
         d.push('0');
--- projects/06/hack_assembler/src/main.rs
+++ projects/06/hack_assembler/src/main.rs
@@ -1,25 +1,64 @@
+use std::io;
 use std::io::Read;
+use std::io::Write;
 use std::fs::File;
+use std::env;
+
+extern crate getopts;
+use getopts::Options;
 
 pub mod parser;
 pub mod code;
 
 fn main() {
-    let asm = read_source("../pong/PongL.asm".to_string());
+    let args: Vec<String> = env::args().collect();
+    let mut opts = Options::new();
+    opts.optopt("o", "", "set output file name", "NAME");
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => { m }
+        Err(f) => { panic!(f.to_string()) }
+    };
+
+    let in_fname = if !matches.free.is_empty() {
+        matches.free[0].clone()
+    } else {
+        println!("Incorrect usage.");
+        return;
+    };
+    let out_fname = match matches.opt_str("o") {
+        Some(name) => name,
+        None => in_fname.replace(".asm", ".hack")
+    };
+
+
+    let asm = read_source(in_fname);
     let instructions = parser::parse_asm(asm);
     //println!("{:?}", instructions);
 
     let mut it = instructions.iter();
+    let mut output = String::new();
     loop {
         match it.next() {
-            Some(x) =>  println!("{}", code::Code::new(x).to_string()),
+            Some(x) =>  {
+                output.push_str(&code::Code::new(x).to_string());
+                output.push_str("\n");
+            }
             None => break
         }
     }
+
+    write_out(out_fname, output);
+    //println!("{}", output);
 }
 
 fn read_source(filename: String) -> String {
     let mut str = String::new();
     File::open(filename).unwrap().read_to_string(&mut str).unwrap();
     str
+}
+
+fn write_out(filename: String, out: String) ->io::Result<()> {
+    let mut file = try!(File::create(filename));
+    file.write_all((&out).as_bytes())
+
 }
